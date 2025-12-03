2d8b4f565f389e398e9e1ae736d95781795055b4
MatiasBSC <127121844+MatiasBSC@users.noreply.github.com>
2025-12-02 22:15:55 -0300

    Ajustes apk de prueba

--- frontend/android/app/src/main/java/com/matiasbsc/frontend/MainActivity.kt
+++ frontend/android/app/src/main/java/com/matiasbsc/frontend/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.frontend
+package com.matiasbsc.frontend
 
 import android.os.Build
 import android.os.Bundle
--- frontend/android/app/src/main/java/com/matiasbsc/frontend/MainApplication.kt
+++ frontend/android/app/src/main/java/com/matiasbsc/frontend/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.frontend
+package com.matiasbsc.frontend
 
 import android.app.Application
 import android.content.res.Configuration
